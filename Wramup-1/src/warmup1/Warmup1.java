89e27a50b588ed5f196bd20b13c99bf528c29452
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-01-10 23:10:42 -0300

    Entered date and time

--- Wramup-1/src/warmup1/Warmup1.java
+++ Wramup-1/src/warmup1/Warmup1.java
@@ -1,13 +1,19 @@
 package warmup1;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Warmup1 {
     public static void main(String[] args) {
+        //Shows the day and time at the moment the program is executed
+        ZonedDateTime dateTime = ZonedDateTime.now(ZoneId.of("America/Sao_Paulo")).
+                format(DateTimeFormatter.ofPattern());
+        System.out.println(dateTime);
+
         boolean isWeekday = false;
         boolean isVacation = false;
         boolean sleepIn = false;
 
-        if(sleepIn == true){
-
-        }
     }
 }
