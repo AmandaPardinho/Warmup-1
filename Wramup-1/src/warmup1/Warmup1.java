f603856170f007a527957b003ab8ed04d4c556b2
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-01-11 16:58:53 -0300

    Improved date and time formatting

--- Wramup-1/src/warmup1/Warmup1.java
+++ Wramup-1/src/warmup1/Warmup1.java
@@ -8,8 +8,8 @@ public class Warmup1 {
     public static void main(String[] args) {
         //Shows the day and time at the moment the program is executed
         String dateTime = ZonedDateTime.now(ZoneId.of("America/Sao_Paulo")).
-                format(DateTimeFormatter.ofPattern("dd.MM.yyyy, hh.mm.ss a"));
-        System.out.println(dateTime);
+                format(DateTimeFormatter.ofPattern("\ndd.MM.yyyy\nhh:mm:ss a"));
+        System.out.println("\nDay and time now: " + dateTime);
 
         boolean isWeekday = false;
         boolean isVacation = false;
