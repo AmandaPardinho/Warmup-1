ee89daf0d5b99d6cf184ccc25cfc2e4930b05f1c
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-01-11 17:37:35 -0300

    Added the exercise statement

--- Wramup-1/src/warmup1/Warmup1.java
+++ Wramup-1/src/warmup1/Warmup1.java
@@ -6,6 +6,12 @@ import java.time.format.DateTimeFormatter;
 
 public class Warmup1 {
     public static void main(String[] args) {
+        System.out.println("""
+                Suppose you are a teacher. The parameter weekday is true if it is a weekday, and the parameter vacation 
+                is true if you are on vacation. Teachers sleep in if it is not a weekday or they're on vacation. Return 
+                true if you sleep in today.
+                """);
+
         //Shows the day and time at the moment the program is executed
         String dateTime = ZonedDateTime.now(ZoneId.of("America/Sao_Paulo")).
                 format(DateTimeFormatter.ofPattern("\ndd.MM.yyyy\nhh:mm:ss a"));
