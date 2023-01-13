e7a364b93f57fccd85811d21e479f1a547eb0fcd
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-01-12 22:05:44 -0300

    Improving the statement

--- Wramup-1/src/warmup1/Warmup1.java
+++ Wramup-1/src/warmup1/Warmup1.java
@@ -1,5 +1,6 @@
 package warmup1;
 
+import java.util.Scanner;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
@@ -9,8 +10,13 @@ public class Warmup1 {
         System.out.println("""
                 Suppose you are a teacher. The parameter weekday is true if it is a weekday, and the parameter vacation 
                 is true if you are on vacation. Teachers sleep in if it is not a weekday or they're on vacation. Return 
-                true if you sleep in today.
+                true if you sleep in today.\n
                 """);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("""
+        Choose the number that represents your country's time zone or the one nearest to it:
+        
+        """);
 
         //Shows the day and time at the moment the program is executed
         String dateTime = ZonedDateTime.now(ZoneId.of("America/Sao_Paulo")).
