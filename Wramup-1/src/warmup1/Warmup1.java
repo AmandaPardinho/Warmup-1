e1029711c0a58a4a6fc74be0111b629c6e26c2f9
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-01-12 22:10:39 -0300

    Adding the time zones present in the Java documentation

--- Wramup-1/src/warmup1/Warmup1.java
+++ Wramup-1/src/warmup1/Warmup1.java
@@ -14,9 +14,33 @@ public class Warmup1 {
                 """);
         Scanner scanner = new Scanner(System.in);
         System.out.println("""
-        Choose the number that represents your country's time zone or the one nearest to it:
-        
-        """);
+                Choose the number that represents your country's time zone or the one nearest to it:
+                        ACT - Australia/Darwin
+                        AET - Australia/Sydney
+                        AGT - America/Argentina/Buenos_Aires
+                        ART - Africa/Cairo
+                        AST - America/Anchorage
+                        BET - America/Sao_Paulo
+                        BST - Asia/Dhaka
+                        CAT - Africa/Harare
+                        CNT - America/St_Johns
+                        CST - America/Chicago
+                        CTT - Asia/Shanghai
+                        EAT - Africa/Addis_Ababa
+                        ECT - Europe/Paris
+                        IET - America/Indiana/Indianapolis
+                        IST - Asia/Kolkata
+                        JST - Asia/Tokyo
+                        MIT - Pacific/Apia
+                        NET - Asia/Yerevan
+                        NST - Pacific/Auckland
+                        PLT - Asia/Karachi
+                        PNT - America/Phoenix
+                        PRT - America/Puerto_Rico
+                        PST - America/Los_Angeles
+                        SST - Pacific/Guadalcanal
+                        VST - Asia/Ho_Chi_Minh
+                """);
 
         //Shows the day and time at the moment the program is executed
         String dateTime = ZonedDateTime.now(ZoneId.of("America/Sao_Paulo")).
