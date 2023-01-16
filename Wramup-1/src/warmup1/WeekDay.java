6613ea434f01408c291f8ee0e33aff5ce55e6ecd
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-01-16 16:43:33 -0300

    Creating constructor, getters and setters

--- Wramup-1/src/warmup1/WeekDay.java
+++ Wramup-1/src/warmup1/WeekDay.java
@@ -5,5 +5,24 @@ public class WeekDay {
     private String zoneId;
     private String dateTime;
 
+    public WeekDay(String zoneId, String dateTime) {
+        this.zoneId = zoneId;
+        this.dateTime = dateTime;
+    }
 
+    public String getZoneId() {
+        return zoneId;
+    }
+
+    public void setZoneId(String zoneId) {
+        this.zoneId = zoneId;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
 }
