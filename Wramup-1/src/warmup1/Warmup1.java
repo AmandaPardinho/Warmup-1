f8ada4adde096c5a41fd2a2b9d836d2b3e937256
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-01-16 16:18:04 -0300

    Refactored class to organize the code

--- Wramup-1/src/warmup1/Warmup1.java
+++ Wramup-1/src/warmup1/Warmup1.java
@@ -19,19 +19,9 @@ public class Warmup1 {
                 vacations between December 15 and January 15, and in the southern hemisphere the longest vacations occur
                 in December and January and short vacations in July.\n
                 """);
+
         Scanner scanner = new Scanner(System.in);
-
-        boolean isWeekDay = false;
-        boolean isVacation = false;
-        boolean sleepIn = false;
         int zone;
-        int year;
-        int month;
-        int day;
-        String zoneId;
-        String dateTime;
-        boolean weekend;
-
 
         System.out.println("""
                 Select the most appropriate number, considering the location of your country in relation to the Equator.
@@ -48,78 +38,23 @@ public class Warmup1 {
 
         switch (zone){
             case 1:
-                zoneId = "America/Chicago";
-                dateTime = String.valueOf(ZonedDateTime.now(ZoneId.of(zoneId))).
-                        format(String.valueOf(DateTimeFormatter.ofPattern("\ndd-MM-yyyy\nhh:mm:ss a")));
-                //day = LocalDateTime.of(dateTime).getDayOfWeek().name();
-                System.out.println("\nDay and time now: " + dateTime);
                 break;
             case 2:
-                zoneId = "America/Sao_Paulo";
-                dateTime = ZonedDateTime.now(ZoneId.of(zoneId)).
-                        format(DateTimeFormatter.ofPattern("\ndd.MM.yyyy\nhh:mm:ss a"));
-                System.out.println("\nDay and time now: " + dateTime);
-                System.out.println("Enter the year:");
-                year = scanner.nextInt();
-                System.out.println("Enter the number of month:");
-                month = scanner.nextInt();
-                System.out.println("Enter the day:");
-                day = scanner.nextInt();
-                //weekDay = LocalDateTime.of(year, month, day, null, null).getDayOfWeek().name();
                 break;
             case 3:
-                zoneId = "Europe/Paris";
-                dateTime = ZonedDateTime.now(ZoneId.of(zoneId)).
-                        format(DateTimeFormatter.ofPattern("\ndd-MM-yyyy\nhh:mm:ss a\n"));
-                TemporalQuery<Boolean> fds = t -> {
-                    DayOfWeek dow = DayOfWeek.from(t);
-                    return dow == DayOfWeek.SATURDAY || dow == DayOfWeek.SUNDAY;
-                };
-                weekend = ZonedDateTime.now().query(fds);
-                System.out.println("\nDay and time now: " + dateTime + weekend);
                 break;
             case 4:
-                zoneId = "Africa/Cairo";
-                dateTime = ZonedDateTime.now(ZoneId.of(zoneId)).
-                        format(DateTimeFormatter.ofPattern("\ndd-MM-yyyy"));
-                System.out.println("\nDay and time now: " + dateTime);
-                //weekend = new SimpleDateFormat(("EEEE")).format(dateTime);
                 break;
             case 5:
-                zoneId = "Africa/Harare";
-                dateTime = String.valueOf(ZonedDateTime.now(ZoneId.of(zoneId)));//.
-                        //format(DateTimeFormatter.ofPattern("\ndd.MM.yyyy\nhh:mm:ss a"));
-                System.out.println("\nDay and time now: " + dateTime);
-                //LocalDateTime dtWeek = LocalDateTime.of(dateTime);
                 break;
             case 6:
-                zoneId = "Asia/Shanghai";
-                dateTime = ZonedDateTime.now(ZoneId.of(zoneId)).
-                        format(DateTimeFormatter.ofPattern("\ndd.MM.yyyy\nhh:mm:ss a"));
-                System.out.println("\nDay and time now: " + dateTime);
                 break;
             case 7:
-                zoneId = "Australia/Sydney";
-                dateTime = ZonedDateTime.now(ZoneId.of(zoneId)).
-                        format(DateTimeFormatter.ofPattern("\ndd-MM-yyyy\nhh:mm:ss a"));
-
-                System.out.println("\nDay and time now: " + dateTime);
                 break;
             default:
                 System.out.println("Invalid number");
         }
 
-        if(isWeekDay == true){
-            if(isVacation == true){
-                sleepIn = true;
-                System.out.println("It's vacation time!");
-            }else {
-                sleepIn = false;
-                System.out.println("Go sleep! Tomorrow you have classes!");
-            }
-        }else{
-            sleepIn = true;
-            System.out.println("It's weekend!");
-        }
+
     }
 }
